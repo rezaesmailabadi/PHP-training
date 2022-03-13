87f34bcd3fda9bc67552d7ebddc57988c9cbb322
rezaesmailabadi <rezaesmailabadi@gmail.com>
2022-03-13 08:45:51 +0330

    php training or the first session

---
+++ index.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+
+<h1>hi mr robot</h1>
+
+
+
+
+<?php
+
+
+// <h1>hi</h1>
+
+
+// echo "<h1>hi</h1>";
+
+
+
+// echo "jflskadjfkljsdf ksld fksld fjksld fjslf js fsjd";
+
+
+
+
+// echo ("hi");
+
+
+
+
+
+
+
+// $mojtab = 20 ;
+
+
+// $h = 3.14;
+
+
+
+// $a = "hi reza";
+
+
+// $reza = 10;
+
+
+// $pass = true  ; 
+
+
+
+
+// echo "$pass";
+
+
+
+
+
+// $name = "ali";
+// $score= 18;
+// $age = 20;
+
+
+// echo ("<h1>student name is : $name </h1>");
+// echo ("<h2>score is : $score</h2>");
+// echo ("<h3>age is  : $age </h3>");
+
+
+
+
+
+// . 
+
+
+
+// echo ("php" . "+" . "mysql" . "=" .  "php deveoper");
+
+
+
+
+
+
+
+
+// $color[0]="red";
+
+// $color[1]="blue";
+
+
+// echo $color[1];
+
+
+
+
+
+// echo 
+// print_r();
+
+
+
+
+
+
+
+
+// $score = array("0" => 20 , "1" => 19 , "2" => 20);
+
+
+
+
+// echo ($score['0']);
+
+
+
+
+
+
+
+
+
+// $color["ali"] = "yello";
+
+
+
+
+// $color = array("ali" => "yello" , "reza" => "red");
+
+
+
+
+
+
+
+// $list['one'] = "PHP";
+// $list['two'] = 18 ;
+
+
+// echo ("<p>نمره ی درس  : {$list['one']} </p>")
+ 
+
+
+
+
+
+
+// define("PI",3.14);
+
+
+// $r = 10; 
+
+// $area = PI*$r*$r;
+
+
+// echo ("circle area : " . $area);
+
+
+
+
+
+
+
+
+
+// if اگر 
+
+
+
+
+
+// $score = 4 ;
+
+// if ($score > 10 ){
+//     echo "you pass the exam";
+// }
+// else{
+//     echo "mardod";
+// }
+
+
+
+
+
+
+
+// $favcolor = "blue";
+
+
+
+// switch ($favcolor) {
+//   case "red":
+//     echo "Your favorite color is red!";
+//     break;
+//   case "blue":
+//     echo "Your favorite color is blue!";
+//     break;
+//   case "green":
+//     echo "Your favorite color is green!";
+//     break;
+//   default:
+//     echo "Your favorite color is neither red, blue, nor green!";
+// }
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+</body>
+
+</html>
